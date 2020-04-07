d6deb72873ce650a81cfcf534712dc35fd8c637a
haruperusii <62590763+haruperusii@users.noreply.github.com>
2020-04-07 11:51:09 +0900

    chat-space

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -74,50 +74,24 @@ $(function(){
       dataType: 'json',
       //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      if (messages.length !== 0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        $('.chat-main__message-list').append(insertHTML);
-        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.chat-main__message-list').append(insertHTML);
+          $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
     if (document.location.href.match(/\/groups\/\d+\/messages/)) {
       setInterval(reloadMessages, 7000);
     }
-  };
   });
-
-  // $('#new_message').on('submit', function(e){
-  //   e.preventDefault();
-  //   var formData = new FormData(this);
-  //   var url = $(this).attr('action');
-  //   $.ajax({
-  //     url: url,
-  //     type: "POST",
-  //     data: formData,
-  //     dataType: 'json',
-  //     processData: false,
-  //     contentType: false
-  //   })
-  //   .done(function(data){
-  //     var html = buildHTML(data);
-  //     $('.chat-main__message-list').append(html);
-  //     $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-  //     $('form')[0].reset();
-  //     $('.form__submit').prop('disabled', false);
-  //   })
-  //   .fail(function() {
-  //     alert("メッセージ送信に失敗しました");
-  //   });
-  // })
-  // if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  //   setInterval(reloadMessages, 7000);
-  // }
 });
+  
